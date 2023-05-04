b04794bd520e1df69168f605a4e7da5f57b10d6c
TakWolf <takwolf@foxmail.com>
2023-05-04 17:43:58 +0800

    添加读取 demo

--- examples/load_bdf.py
+++ examples/load_bdf.py
@@ -6,6 +6,10 @@ from examples import path_define
 
 def main():
     font = bdffont.load_bdf(os.path.join(path_define.assets_dir, 'unifont-15.0.01.bdf'))
+    for code_point, glyph in font.code_point_to_glyph.items():
+        print(f"---> {chr(code_point)} {code_point:04X}")
+        for bitmap_row in glyph.bitmap:
+            print(''.join(map(str, bitmap_row)).replace('0', '  ').replace('1', '##'))
 
 
 if __name__ == '__main__':
