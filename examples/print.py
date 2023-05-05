55a12ab953c1e21adc4d42fd912d4cf9f1e0f380
TakWolf <takwolf@foxmail.com>
2023-05-05 15:42:34 +0800

    添加打印例子

---
+++ examples/print.py
@@ -0,0 +1,19 @@
+import os
+
+import bdffont
+from examples import assets_dir
+
+
+def main():
+    font = bdffont.load_bdf(os.path.join(assets_dir, 'unifont-15.0.01.bdf'))
+    alphabet = list(font.code_point_to_glyph.items())
+    alphabet.sort()
+    for code_point, glyph in alphabet:
+        print(f'--> {code_point}')
+        for bitmap_row in glyph.bitmap:
+            print(''.join(map(str, bitmap_row)).replace('0', '__').replace('1', '##'))
+        print()
+
+
+if __name__ == '__main__':
+    main()
