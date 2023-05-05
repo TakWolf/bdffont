d7583962817223e0b71ae929045337c50dcbc4f4
TakWolf <takwolf@foxmail.com>
2023-05-05 10:26:58 +0800

    调整注释

--- src/bdffont/glyph.py
+++ src/bdffont/glyph.py
@@ -8,30 +8,25 @@ class BdfGlyph:
     # Example: 39
     code_point: int
 
-    # The scalable width in x and y of character.
-    # Scalable widths are in units of 1/1000th of the size of the character.
-    # If the size of the character is p points, the width information must be scaled by p/1000 to get
-    # the width of the character in printer’s points.
-    # This width information should be considered as a vector indicating the position of the next
-    # character’s origin relative to the origin of this character.
+    # The scalable width in x and y of character. Scalable widths are in units of 1/1000th of the size of
+    # the character. If the size of the character is p points, the width information must be scaled by p/1000
+    # to get the width of the character in printer’s points. This width information should be considered as a
+    # vector indicating the position of the next character’s origin relative to the origin of this character.
     # To convert the scalable width to the width in device pixels, multiply s_width times p/1000 times r/72,
-    # where r is the device resolution in pixels per inch.
-    # The result is a real number giving the ideal print width in device pixels.
-    # The actual device width must of course be an integral number of device pixels and is given in the next entry.
-    # The s_width y value should always be zero for a standard X font.
+    # where r is the device resolution in pixels per inch. The result is a real number giving the ideal print
+    # width in device pixels. The actual device width must of course be an integral number of device pixels and
+    # is given in the next entry. The s_width y value should always be zero for a standard X font.
     s_width: (int, int)
 
-    # The width in x and y of the character in device units.
-    # Like the s_width, this width information is a vector indicating the position of the next character’s origin
-    # relative to the origin of this character.
-    # Note that the d_width of a given "hand-tuned" WYSIWYG glyph may deviate slightly from its ideal
-    # device-independent width given by s_width in order to improve its typographic characteristics
-    # on a display.
-    # The d_width y value should always be zero for a standard X font.
+    # The width in x and y of the character in device units. Like the s_width, this width information is a vector
+    # indicating the position of the next character’s origin relative to the origin of this character. Note that
+    # the d_width of a given "hand-tuned" WYSIWYG glyph may deviate slightly from its ideal device-independent width
+    # given by s_width in order to improve its typographic characteristics on a display. The d_width y value should
+    # always be zero for a standard X font.
     d_width: (int, int)
 
-    # The width in x (BBw), height in y (BBh), and x and y displacement (BBox, BBoy) of the lower left corner
-    # from the origin of the character.
+    # The width in x (BBw), height in y (BBh), and x and y displacement (BBox, BBoy) of the lower left corner from
+    # the origin of the character.
     bbx: (int, int, int, int)
 
     # The bitmap object.
