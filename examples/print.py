911c0289036473992eac60dbbfee35dcec570e49
TakWolf <takwolf@foxmail.com>
2023-05-05 21:58:02 +0800

    print demo 输出字形 bounding_box

--- examples/print.py
+++ examples/print.py
@@ -9,7 +9,7 @@ def main():
     alphabet = list(font.code_point_to_glyph.items())
     alphabet.sort()
     for code_point, glyph in alphabet:
-        print(f'--> {code_point}')
+        print(f'--> {code_point} {glyph.bounding_box}')
         for bitmap_row in glyph.bitmap:
             print(''.join(map(str, bitmap_row)).replace('0', '__').replace('1', '##'))
         print()
