15b098549b5083a94fd4c076df69b82399f79ec7
TakWolf <takwolf@foxmail.com>
2023-05-06 10:26:37 +0800

    调整打印例子

--- examples/print.py
+++ examples/print.py
@@ -4,15 +4,21 @@ import bdffont
 from examples import assets_dir
 
 
-def main():
-    font = bdffont.load_bdf(os.path.join(assets_dir, 'unifont-15.0.01.bdf'))
+def print_bdf(file_name: str):
+    font = bdffont.load_bdf(os.path.join(assets_dir, file_name))
     alphabet = list(font.code_point_to_glyph.items())
     alphabet.sort()
+    print(f'##### {file_name}')
     for code_point, glyph in alphabet:
-        print(f'--> {code_point} {glyph.bounding_box}')
+        print(f'-> {code_point} {glyph.bounding_box}')
         for bitmap_row in glyph.bitmap:
-            print(''.join(map(str, bitmap_row)).replace('0', '__').replace('1', '##'))
+            print(''.join(map(str, bitmap_row)).replace('0', '__').replace('1', '**'))
         print()
+
+
+def main():
+    print_bdf('unifont-15.0.01.bdf')
+    print_bdf('galmuri9.bdf')
 
 
 if __name__ == '__main__':
