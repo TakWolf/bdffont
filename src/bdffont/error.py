41ef7f3f8bc31a5ab754511967509350d89a10de
TakWolf <takwolf@foxmail.com>
2023-05-29 16:11:33 +0800

    代码整理

--- src/bdffont/error.py
+++ src/bdffont/error.py
@@ -3,12 +3,6 @@ class BdfException(Exception):
     pass
 
 
-class BdfGlyphExists(BdfException):
-    def __init__(self, code_point: int):
-        self.code_point = code_point
-        super().__init__(f"Glyph '{code_point}' already exists")
-
-
 class BdfMissingLine(BdfException):
     def __init__(self, word: str):
         self.word = word
@@ -21,15 +15,21 @@ class BdfValueIncorrect(BdfException):
         super().__init__(f"'{word}' value incorrect")
 
 
-class BdfIllegalBitmap(BdfException):
-    def __init__(self, code_point: int, reason: str):
-        self.code_point = code_point
-        super().__init__(f"Glyph '{code_point}': {reason}")
-
-
 class BdfIllegalPropertiesKey(BdfException):
     pass
 
 
 class BdfIllegalPropertiesValue(BdfException):
     pass
+
+
+class BdfGlyphExists(BdfException):
+    def __init__(self, code_point: int):
+        self.code_point = code_point
+        super().__init__(f"Glyph '{code_point}' already exists")
+
+
+class BdfIllegalBitmap(BdfException):
+    def __init__(self, code_point: int, reason: str):
+        self.code_point = code_point
+        super().__init__(f"Glyph '{code_point}': {reason}")
