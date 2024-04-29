d5d62a91794002881c75b043463bfc0b0b68c767
TakWolf <takwolf@foxmail.com>
2024-04-29 14:39:47 +0800

    代码整理

--- src/bdffont/error.py
+++ src/bdffont/error.py
@@ -9,42 +9,42 @@ class BdfParseError(BdfError):
 
 class BdfMissingLineError(BdfError):
     def __init__(self, word: str):
-        self.word = word
         super().__init__(f"Missing line: '{word}'")
+        self.word = word
 
 
 class BdfIllegalWordError(BdfError):
     def __init__(self, line_num: int, word: str):
+        super().__init__(f"[line {line_num}] Illegal word: '{word}'")
         self.line_num = line_num
         self.word = word
-        super().__init__(f"[line {line_num}] Illegal word: '{word}'")
 
 
 class BdfCountError(BdfError):
     def __init__(self, word: str, expected: int, actual: int):
+        super().__init__(f"'{word}' expected to be {expected} but actually {actual}")
         self.word = word
         self.expected = expected
         self.actual = actual
-        super().__init__(f"'{word}' expected to be {expected} but actually {actual}")
 
 
 class BdfPropKeyError(BdfError):
     def __init__(self, key: str, reason: str):
+        super().__init__(f"'{key}': {reason}")
         self.key = key
         self.reason = reason
-        super().__init__(f"'{key}': {reason}")
 
 
 class BdfPropValueError(BdfError):
     def __init__(self, key: str, value: object, reason: str):
+        super().__init__(f"'{key}': '{value}': {reason}")
         self.key = key
         self.value = value
         self.reason = reason
-        super().__init__(f"'{key}': '{value}': {reason}")
 
 
 class BdfXlfdError(BdfError):
     def __init__(self, font_name: str, reason: str):
+        super().__init__(f"'{font_name}': {reason}")
         self.font_name = font_name
         self.reason = reason
-        super().__init__(f"'{font_name}': {reason}")
