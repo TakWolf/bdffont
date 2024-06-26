f1d857e584230e0e51ffbef026ce6bafdd4f0c31
TakWolf <takwolf@foxmail.com>
2024-06-26 11:09:36 +0800

    命名调整

--- tests/test_load_save.py
+++ tests/test_load_save.py
@@ -11,40 +11,40 @@ def _file_sha256(file_path: Path) -> str:
 
 
 def test_unifont(tmp_path: Path):
-    load_file_path = project_root_dir.joinpath('assets', 'unifont', 'unifont-15.1.05.bdf')
-    save_file_path = tmp_path.joinpath('unifont-15.1.05.bdf')
-    font = BdfFont.load(load_file_path)
-    save_file_path.write_text(font.dump().replace('\nBITMAP\n', '\nBITMAP \n'), 'utf-8')
-    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+    load_path = project_root_dir.joinpath('assets', 'unifont', 'unifont-15.1.05.bdf')
+    save_path = tmp_path.joinpath('unifont-15.1.05.bdf')
+    font = BdfFont.load(load_path)
+    save_path.write_text(font.dump().replace('\nBITMAP\n', '\nBITMAP \n'), 'utf-8')
+    assert _file_sha256(load_path) == _file_sha256(save_path)
 
 
 def test_galmuri9(tmp_path: Path):
-    load_file_path = project_root_dir.joinpath('assets', 'galmuri', 'galmuri9.bdf')
-    save_file_path = tmp_path.joinpath('galmuri9.bdf')
-    font = BdfFont.load(load_file_path)
-    font.save(save_file_path)
-    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+    load_path = project_root_dir.joinpath('assets', 'galmuri', 'galmuri9.bdf')
+    save_path = tmp_path.joinpath('galmuri9.bdf')
+    font = BdfFont.load(load_path)
+    font.save(save_path)
+    assert _file_sha256(load_path) == _file_sha256(save_path)
 
 
 def test_misaki_gothic(tmp_path: Path):
-    load_file_path = project_root_dir.joinpath('assets', 'misaki', 'misaki_gothic.bdf')
-    save_file_path = tmp_path.joinpath('misaki_gothic.bdf')
-    font = BdfFont.load(load_file_path)
-    font.save(save_file_path)
-    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+    load_path = project_root_dir.joinpath('assets', 'misaki', 'misaki_gothic.bdf')
+    save_path = tmp_path.joinpath('misaki_gothic.bdf')
+    font = BdfFont.load(load_path)
+    font.save(save_path)
+    assert _file_sha256(load_path) == _file_sha256(save_path)
 
 
 def test_misaki_gothic_2nd(tmp_path: Path):
-    load_file_path = project_root_dir.joinpath('assets', 'misaki', 'misaki_gothic_2nd.bdf')
-    save_file_path = tmp_path.joinpath('misaki_gothic_2nd.bdf')
-    font = BdfFont.load(load_file_path)
-    font.save(save_file_path)
-    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+    load_path = project_root_dir.joinpath('assets', 'misaki', 'misaki_gothic_2nd.bdf')
+    save_path = tmp_path.joinpath('misaki_gothic_2nd.bdf')
+    font = BdfFont.load(load_path)
+    font.save(save_path)
+    assert _file_sha256(load_path) == _file_sha256(save_path)
 
 
 def test_misaki_mincho(tmp_path: Path):
-    load_file_path = project_root_dir.joinpath('assets', 'misaki', 'misaki_mincho.bdf')
-    save_file_path = tmp_path.joinpath('misaki_mincho.bdf')
-    font = BdfFont.load(load_file_path)
-    font.save(save_file_path)
-    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+    load_path = project_root_dir.joinpath('assets', 'misaki', 'misaki_mincho.bdf')
+    save_path = tmp_path.joinpath('misaki_mincho.bdf')
+    font = BdfFont.load(load_path)
+    font.save(save_path)
+    assert _file_sha256(load_path) == _file_sha256(save_path)
